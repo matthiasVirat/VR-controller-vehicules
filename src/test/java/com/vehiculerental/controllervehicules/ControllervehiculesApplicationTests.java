8711859d9be28951d3710734868c01d0844adf15
matthiasVirat <virat.matthias@gmail.com>
2019-10-28 10:10:45 +0100

    initial commit

---
+++ src/test/java/com/vehiculerental/controllervehicules/ControllervehiculesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehiculerental.controllervehicules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllervehiculesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
